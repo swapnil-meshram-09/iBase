8cdcea8155bcbe977e7522626993c3292528d3d9
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-23 18:38:12 +0530

    new commit

---
+++ student_enrollemnt/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = ""; // replace with your MySQL root password
+$dbname = "registration_db";
+
+$conn = mysqli_connect($host, $user, $password, $dbname);
+
+if (!$conn) {
+    die("Database Connection Failed: " . mysqli_connect_error());
+}
+?>
