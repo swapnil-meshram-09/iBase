0a37b62b0a2e0be8c9afa906fa56605ebc948b08
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-23 18:47:36 +0530

    new commit

--- form/welcome.php
+++ form/welcome.php
@@ -3,7 +3,7 @@ session_start();
 include "db.php";
 
 if (!isset($_SESSION['last_id'])) {
-    header("Location: index.html");
+    header("Location: index.php");
     exit();
 }
 
@@ -19,50 +19,54 @@ if (!$result) {
 $data = mysqli_fetch_assoc($result);
 
 // Convert Date Format
-$start_date = date("d - m - Y", strtotime($data['start_date']));
-$end_date = date("d - m - Y", strtotime($data['end_date']));
+$start_date = date("d-m-Y", strtotime($data['start_date']));
+$end_date = date("d-m-Y", strtotime($data['end_date']));
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Welcome</title>
+<title>Welcome</title>
 
-    <style>
-        body {
-            background: #dde3ea;
-            font-family: Arial, sans-serif;
-        }
+<style>
 
-        .box {
-            width: 400px;
-            background: white;
-            margin: 70px auto;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0px 0px 10px #aaa;
-        }
+body {
+    background: #dde3ea;
+    font-family: Arial, sans-serif;
+}
 
-        h1 {
-            text-align: center;
-            margin-bottom: 20px;
-        }
+.box {
+    width: 400px;
+    background: white;
+    margin: 70px auto;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px #aaa;
+}
 
-        p {
-            font-size: 15px;
-        }
-    </style>
+h1 {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+p {
+    font-size: 15px;
+}
+
+</style>
 </head>
 
 <body>
 
 <div class="box">
-    <h1>Welcome</h1>
 
-    <p><b>Title:</b> <?php echo $data['title']; ?></p>
-    <p><b>Description:</b> <?php echo $data['description']; ?></p>
-    <p><b>Start Date:</b> <?php echo $start_date; ?></p>
-    <p><b>End Date:</b> <?php echo $end_date; ?></p>
+<h1>Welcome</h1>
+
+<p><b>Title:</b> <?php echo $data['title']; ?></p>
+<p><b>Description:</b> <?php echo $data['description']; ?></p>
+<p><b>Start Date:</b> <?php echo $start_date; ?></p>
+<p><b>End Date:</b> <?php echo $end_date; ?></p>
+
 </div>
 
 </body>
