f84fd288d246d84d99112f552361eff8e2c48d16
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-27 14:31:58 +0530

    new commit

---
+++ courese/coures.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course</title>
+
+    <div>
+        <form action="">
+
+
+        </form>
+
+        <form action="">
+
+
+        </form>
+    </div>
+
+    <style>
+
+
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
