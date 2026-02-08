8082425b668e1987bf99632817b0ddd0459f8737
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-08 16:34:08 +0530

    new commit

---
+++ project/student/dashboard.php
@@ -0,0 +1,90 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background: #dde3ea;
+            margin: 0px;
+        }
+
+        #formBox {
+            width: 400px;
+            margin: auto;
+            background: white;
+            padding: 25px;
+            margin-top: 60px;
+            border-radius: 15px;
+            box-shadow: 0px 0px 10px #aaa;
+            font-size: 15px;
+        }
+
+    h2 {
+    text-align: center;
+    margin-top: 0px;
+    margin-bottom: 25px;
+    font-size: 24px;
+}
+
+label {
+    font-weight: bold;
+    margin-top: 10px;
+    display: block;
+    margin-left: 10px;
+    
+}
+
+input, select {
+    padding: 8px 12px;
+    margin-left:10px;
+    border: none;
+    border-radius: 6px;
+    background: #f2f2f2;
+    width: 90%;
+    margin-top: 20px;
+}
+
+select {
+    width: 95%;
+}
+
+button {
+    margin-top: 15px;
+    padding: 12px;
+    background: #16a34a;
+    border: none;
+    color: white;
+    border-radius: 10px;
+    width: 95%;
+    font-size: 16px;
+    cursor: pointer;
+    margin-left: 10px;
+}
+
+button:hover {
+    background: green;
+}
+
+.error {
+    text-align: center;
+    color: red;
+    font-weight: bold;
+}
+        .success {
+            color: green;
+            text-align: center;
+            font-weight: bold;
+        }
+    </style>
+
+</head>
+<body>
+
+<form method="POST" id="formBox">
+    <h2>Dashboard</h2>
+
+</form>
+
+</body>
+</html>
