894c94dd2f6fa30261a0d9719287eda403397367
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-01 18:26:42 +0530

    new commit

--- project/user/userLogin.php
+++ project/user/userLogin.php
@@ -154,7 +154,7 @@ button:hover { background:#12833b; }
 
 <body>
 
-<div class="tabs">
+<!-- <div class="tabs">
     <a class="tab <?= $currentTab=='main.php' ? 'active' : '' ?>" href="main.php">Home</a>
     <a class="tab <?= $currentTab=='studentLogin/login.php' ? 'active' : '' ?>" href="studentLogin/login.php">Student Login</a>
     <a class="tab <?= $currentTab=='user/userLogin.php' ? 'active' : '' ?>" href="user/userLogin.php">User Login</a>
@@ -163,6 +163,34 @@ button:hover { background:#12833b; }
     <a class="tab <?= $currentTab=='addfaculty/addFaculty.php' ? 'active' : '' ?>" href="addfaculty/addFaculty.php"> Add Faculty</a>
     <a class="tab <?= $currentTab=='viewStudent/viewStudent.php' ? 'active' : '' ?>" href="viewStudent/viewStudent.php"> View Student</a>
     <a class="tab <?= $currentTab=='viewFaculty/viewFaculty.php' ? 'active' : '' ?>" href="viewFaculty/viewFaculty.php"> View Faculty</a>
+</div> -->
+
+<div class="tabs">
+
+    <a class="tab <?= $currentTab=='main.php' ? 'active' : '' ?>" 
+       href="<?= $baseURL ?>main.php">Home</a>
+
+    <a class="tab <?= $currentTab=='login.php' ? 'active' : '' ?>" 
+       href="<?= $baseURL ?>studentLogin/login.php">Student Login</a>
+
+    <a class="tab <?= $currentTab=='userLogin.php' ? 'active' : '' ?>" 
+       href="<?= $baseURL ?>user/userLogin.php">User Login</a>
+
+    <a class="tab <?= $currentTab=='addStudent.php' ? 'active' : '' ?>" 
+       href="<?= $baseURL ?>addStudent/addStudent.php">Add Student</a>
+
+    <a class="tab <?= $currentTab=='addUserRegistration.php' ? 'active' : '' ?>" 
+       href="<?= $baseURL ?>addUser/addUserRegistration.php">Add User</a>
+
+    <a class="tab <?= $currentTab=='addFaculty.php' ? 'active' : '' ?>" 
+       href="<?= $baseURL ?>addfaculty/addFaculty.php">Add Faculty</a>
+
+    <a class="tab <?= $currentTab=='viewStudent.php' ? 'active' : '' ?>" 
+       href="<?= $baseURL ?>viewStudent/viewStudent.php">View Student</a>
+
+    <a class="tab <?= $currentTab=='viewFaculty.php' ? 'active' : '' ?>" 
+       href="<?= $baseURL ?>viewFaculty/viewFaculty.php">View Faculty</a>
+
 </div>
 
 <form method="POST" id="formBox" autocomplete="off">
